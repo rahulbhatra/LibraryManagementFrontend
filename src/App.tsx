ae9098d862c1e05028f9e53a12a5c33f02b00379
Rahul Sharma <rsbhatra98@gmail.com>
2022-11-06 02:04:57 -0600

    pushing all files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import SignUp from './components/signup/signup";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SignUp />
+      </header>
+    </div>
+  );
+}
+
+export default App;
