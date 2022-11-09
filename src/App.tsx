91cb399fadff51aa78f9ba8b8097cd227e7fbaf6
Rahul Sharma <rsbhatra98@gmail.com>
2022-11-08 23:46:27 -0600

    token service added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+<<<<<<< HEAD
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/dashboard/dashboard';
 import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes';
 import SignIn from './components/signin/signin';
+=======
+import './App.css';
+>>>>>>> 8a0fa49 (initial setup)
 import SignUp from './components/signup/signup';
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<SignIn />}/>
@@ -19,6 +24,11 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+=======
+      <header className="App-header">
+        <SignUp />
+      </header>
+>>>>>>> 8a0fa49 (initial setup)
     </div>
   );
 }
--- src/components/signup/signup.tsx
+++ src/components/signup/signup.tsx
@@ -12,14 +12,31 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+<<<<<<< HEAD
 import useAxios from 'axios-hooks';
 import { useEffect, useState } from 'react';
 import CustomSnackbar from '../snackbar/snackbar';
 import { AlertColor } from '@mui/material';
+=======
+
+const Copyright = (props: any) => {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+>>>>>>> 8a0fa49 (initial setup)
 
 const theme = createTheme();
 
 const SignUp = () => {
+<<<<<<< HEAD
   const [{data, loading, error}] = useAxios(
     'http://localhost:8080/user'
   );
@@ -40,6 +57,8 @@ const SignUp = () => {
     }
   }, [userError]);
 
+=======
+>>>>>>> 8a0fa49 (initial setup)
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -47,6 +66,7 @@ const SignUp = () => {
       email: data.get('email'),
       password: data.get('password'),
     });
+<<<<<<< HEAD
     insertUser({
       data: {
         username: data.get('email'),
@@ -140,6 +160,99 @@ const SignUp = () => {
         </Box>
       </Box>
     </Container>
+=======
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+>>>>>>> 8a0fa49 (initial setup)
   );
 };
 
